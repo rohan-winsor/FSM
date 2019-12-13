917ebaf49c4dd0ff019ab0c4fbedd0356c72c5b3
ahnor <wrc071@gmail.com>
2019-12-13 07:03:34 +0530

    README update

--- babyYoda.py
+++ babyYoda.py
@@ -1,5 +1,7 @@
 from enum import Enum, unique
 from random import randint
+import time 
+
 
 @unique
 class STATE(Enum):
@@ -36,18 +38,22 @@ class BabyYoda(Fsm):
     def sleeping(self):
         if self.capactiy + 20 <= 100:
             self.capactiy += 20
-    def printState(self,list1):
+
+    def printState(self, list1):
         # print(list1)
         print(list1[1])
         # pass
 
 
 if __name__ == "__main__":
-    all_states = {1: [STATE.EAT,"Eating...."], 2:[STATE.SLEEP,"Sleeping..."], 3: [STATE.FORCE,"Using Force..."], 4: [STATE.IDLE,"Cooing....."]}
+    all_states = {1: [STATE.EAT, "Eating...."], 2: [STATE.SLEEP, "Sleeping..."], 3: [\
+        STATE.FORCE, "Using Force..."], 4: [STATE.IDLE, "Cooing....."]}
     babyyoda = BabyYoda()
     count = 0
-    while babyyoda.capactiy >= 0:
-        s = randint(1,4)
+    while babyyoda.capactiy > 0:
+        time.sleep(2)
+        print("Health ::", babyyoda.capactiy)
+        s = randint(1, 4)
         babyyoda.set_state(all_states[s][0])
         if babyyoda.get_state() == all_states[3][0]:
             babyyoda.useForce()
@@ -64,4 +70,4 @@ if __name__ == "__main__":
         else:
             break
         count += 1
-    print(count)
+    print("Number of Inter :",count)
